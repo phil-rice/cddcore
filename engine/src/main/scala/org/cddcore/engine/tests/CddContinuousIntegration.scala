6294224c24aeb3d527a7c49d0eebdee3dec0fa31
Phil <phil.rice@iee.org>
2014-02-02 18:27:07 +0000

    Continuous Integration now produces reports

--- engine/src/main/scala/org/cddcore/engine/tests/CddContinuousIntegration.scala
+++ engine/src/main/scala/org/cddcore/engine/tests/CddContinuousIntegration.scala
@@ -15,6 +15,17 @@ class CddContinuousIntegrationRunner(val clazz: Class[Any]) extends CddRunner {
   def title = "Constraint Driven Development"
   val instance = Engine.test { instantiate(clazz) }.asInstanceOf[HasEngines];
   lazy val enginesToNameMap = Map(instance.engines.map((e) => (e, e.titleOrDescription("<No Name>"))): _*)
+  val rootEngines = instance.engines
+  rootEngines.headOption match {
+    case Some(engine) =>
+      val packageObject = clazz.getPackage()
+      val packageName = if (packageObject == null) "default package" else packageObject.getName()
+      val project = Project(packageName + "." + clazz.getSimpleName, rootEngines: _*)
+      val report = Report("Junit", project)
+      val logger = engine.asInstanceOf[EngineWithLogger].logger
+      ReportCreator.fileSystem(logger, report).create
+    case _ =>
+  }
 }
 
 @RunWith(classOf[CddContinuousIntegrationRunner])
