72b9f0f63a0c6f753098e52a6022445aed261e67
Phil <phil.rice@iee.org>
2014-10-31 11:31:51 +0000

    Preparing 2.1.2

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import com.typesafe.sbteclipse.plugin.EclipsePlugin.EclipseKeys
 
 object CddBuild extends Build {
 
-  val cddVersionNo = "2.1.1"
+  val cddVersionNo = "2.1.2"
   val scalaVersionNo = "2.10.4"
   val scalaTestVersionNo = "2.2.0"
     
